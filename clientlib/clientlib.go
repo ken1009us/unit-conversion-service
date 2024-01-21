d5e33e56eac558f97a64b7527f94aec6dd3cd2dd
Ken Wu <kenwu1009us@gmail.com>
2024-01-21 08:50:36 -0800

    singleton grpc client

--- clientlib/clientlib.go
+++ clientlib/clientlib.go
@@ -4,46 +4,55 @@ package clientlib
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	pb "github.com/ken1009us/unit-conversion-service/pb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/status"
 )
 
 const address = "localhost:50051"
 
+var (
+	clientInstance *UnitConversionClient
+	once           sync.Once
+)
+
 type UnitConversionClient struct {
-    conn   *grpc.ClientConn
-    client pb.UnitConversionServiceClient
+	conn   *grpc.ClientConn
+	client pb.UnitConversionServiceClient
 }
 
 func NewUnitConversionClient() (*UnitConversionClient, error) {
-    conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
-    if err != nil {
-        return nil, fmt.Errorf("failed to connect to the server: %v", err)
-    }
-    client := pb.NewUnitConversionServiceClient(conn)
-    return &UnitConversionClient{conn: conn, client: client}, nil
-}
-
-func (ucc *UnitConversionClient) Close() {
-    ucc.conn.Close()
+	var err error
+	once.Do(func() {
+		conn, connErr := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+		if connErr != nil {
+			err = fmt.Errorf("failed to connect to the server: %v", connErr)
+			return
+		}
+		client := pb.NewUnitConversionServiceClient(conn)
+		clientInstance = &UnitConversionClient{conn: conn, client: client}
+	})
+	if err != nil {
+		return nil, err
+	}
+	return clientInstance, nil
 }
 
 func (ucc *UnitConversionClient) Convert(ctx context.Context, fromUnit, toUnit string, value float64) (float64, error) {
-    r, err := ucc.client.ConvertUnit(ctx, &pb.UnitConversionRequest{FromUnit: fromUnit, ToUnit: toUnit, Value: value})
-    if err != nil {
-        st, ok := status.FromError(err)
-        if !ok {
-            return 0, fmt.Errorf("error during conversion: %v", err)
-        }
-        return 0, fmt.Errorf("conversion error: %s (code: %v)", st.Message(), st.Code())
-    }
+	r, err := ucc.client.ConvertUnit(ctx, &pb.UnitConversionRequest{FromUnit: fromUnit, ToUnit: toUnit, Value: value})
+	if err != nil {
+		return 0, fmt.Errorf("error during conversion: %v", err)
+	}
 
-    if r.GetConvertedValue() == 0 && r.GetError() != "" {
-        return 0, fmt.Errorf("conversion error: %s", r.GetError())
-    }
+	if r.GetConvertedValue() == 0 && r.GetError() != "" {
+		return 0, fmt.Errorf("conversion error: %s", r.GetError())
+	}
 
-    return r.GetConvertedValue(), nil
+	return r.GetConvertedValue(), nil
+}
+
+func (ucc *UnitConversionClient) Close() {
+	ucc.conn.Close()
 }
